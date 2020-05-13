36ab1be5afb34bdcbc26a5cd8eda3a830ebf731d
Anzoni74 <64782927+Anzoni74@users.noreply.github.com>
2020-05-13 16:45:43 +0200

    Videojuego

---
+++ El laberinto de la muerte/El laberinto de la muerte/Assets/Scripts/BallControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    //Variables publicas
+    public float ballForce = 1.0f;
+
+    //Variables privadas
+    private Vector3 horizontalInput, verticalInput;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Vamos a empujar la bola en la direccion que pulse el usuario
+
+        //Calculamos el imput horizontal y vertical
+        horizontalInput = transform.right * Input.GetAxis("Horizontal");
+        verticalInput = transform.forward * Input.GetAxis("Vertical");
+
+        //Segundo empujamos la bola con la fuerza ballForce
+        GetComponent<Rigidbody>().AddForce((horizontalInput + verticalInput) * ballForce);
+	}
+}
